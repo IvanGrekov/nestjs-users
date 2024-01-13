09ae7dbacd5b10c71919e4bce3cf380b020ba6af
Ivan Grekov <ivan.grekov@windmill.digital>
2024-01-13 19:50:11 +0200

    exception filters were moved to folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EverythingExceptionFilter } from './everything-exception.filter';
-import { HttpExceptionFilter } from './http-exception.filter';
+import { EverythingExceptionFilter } from './exception-filters/everything-exception.filter';
+import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
